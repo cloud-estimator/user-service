dd2f851ab9b36b14b1fcd6a3ddd96e6f7823679d
Dharminder Dharna <dharminder@stellar.aero>
2018-05-22 15:13:44 -0700

    - adds cors config

--- src/main/java/cloud/estimator/user/config/ResourceServerConfig.java
+++ src/main/java/cloud/estimator/user/config/ResourceServerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+import cloud.estimator.user.security.AuthoritiesConstants;
+
 
 @Configuration
 @EnableResourceServer
@@ -19,15 +23,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
 		http
-				.exceptionHandling()
-				.authenticationEntryPoint(problemSupport)
-				.accessDeniedHandler(problemSupport)
-				.and()
-				.requestMatchers()
-				.and()
-				.authorizeRequests()
-				.antMatchers("/api/allow", "/api/register", "/api-docs/**").permitAll()
-				.anyRequest().authenticated();
+			.exceptionHandling()
+			.authenticationEntryPoint(problemSupport)
+			.accessDeniedHandler(problemSupport)
+		.and()
+            .csrf()
+            .disable()
+            .headers()
+            .frameOptions()
+            .disable()
+        .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+			.authorizeRequests()
+			.antMatchers("/api/allow").permitAll()
+            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/account/reset-password/init").permitAll()
+            .antMatchers("/api/account/reset-password/finish").permitAll()
+            .antMatchers("/api/profile-info").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .antMatchers("/management/health").permitAll()
+            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN);
+		// @formatter:on
 	}
 }
